058081bd47cf0751ac7fef8632154f524d811503
TheRealJarbean <jaron.e.anderson@gmail.com>
2024-02-13 01:43:12 -0500

    feat: Replace inline tags in note with confgured values
    Implemented using MarkdownPostProcessor in reader view. Currently tags are configured in the GLOBAL_TAGS Record. Identified tags in note are replaced and colored based on whether or not an associated value is found.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
+import { error } from "console";
 import { TFile, Plugin, ItemView } from "obsidian";
+import { text } from "stream/consumers";
+
+const GLOBAL_TAGS: Record<string, string> = {
+	"firstName": "Tyler",
+};
 
 export default class ExamplePlugin extends Plugin {
 
@@ -14,8 +20,26 @@ export default class ExamplePlugin extends Plugin {
 				console.log(fileContents);
 				navigator.clipboard.writeText(fileContents);
 			}
-			
 		});
+
+		this.registerMarkdownPostProcessor((element, context) => {
+			const codeblocks = element.findAll("code");
+
+			for (let codeblock of codeblocks) {
+				const text = codeblock.innerText.trim();
+				console.log("text: " + text);
+				if (text[0] === "+" && text[text.length - 1] === "+") {
+					const tag = text.substring(1, text.length - 1); // Trim identifiers
+					const tagFound: boolean = GLOBAL_TAGS[tag] !== undefined;
+					const replaceEl = codeblock.createSpan({
+						text: tagFound ? GLOBAL_TAGS[tag] : text,
+						cls: tagFound ? "tag__success" : "tag__error",
+					});
+					console.log(replaceEl);
+					codeblock.replaceWith(replaceEl);
+				}
+			}
+		})
 	}
 
 	async onunload() {
