f12200d2ad51e9a67c9d74d58e7bedff508ff755
TheRealJarbean <47651803+TheRealJarbean@users.noreply.github.com>
2024-02-14 00:37:23 -0500

    feat: Implement tag parsing (close #2)
    find_all_unique_tags()
    Returns an array of strings containing all unique tags in a note.
    
    find_and_replace_all_tags()
    Takes text and a Record of tags as input, returns text with the tags replaced with their associated value in the record or TAG_NOT_FOUND.
    
    Implemented a MarkdownPostProcessor to replace tags with values in Reading View. If a tag is found, it is replaced and colored green, if not, it is colored red.

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const openDelimiter = '+';
+export const closeDelimiter = '+';
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
+import { error } from "console";
 import { TFile, Plugin, ItemView } from "obsidian";
+import { text } from "stream/consumers";
+import * as config from "./config";
+import * as utils from "./utils";
+
+const GLOBAL_TAGS: Record<string, string> = {
+	"firstName": "Tyler",
+};
 
 export default class ExamplePlugin extends Plugin {
 
@@ -9,13 +17,33 @@ export default class ExamplePlugin extends Plugin {
 			const currentFile: TFile | null = this.app.workspace.getActiveFile();
 
 			if (currentFile) {
-				console.log(currentFile);
-				const fileContents = await this.app.vault.cachedRead(currentFile);
-				console.log(fileContents);
-				navigator.clipboard.writeText(fileContents);
+				const fileContents: string = await this.app.vault.cachedRead(currentFile);
+				const currentTags = utils.find_all_unique_tags(fileContents, "`" + config.openDelimiter, config.closeDelimiter + "`");
+				console.log(currentTags);
+				const clipboardText: string = utils.find_and_replace_all_tags(fileContents, "`" + config.openDelimiter, config.closeDelimiter + "`", GLOBAL_TAGS);
+				console.log(clipboardText);
+				navigator.clipboard.writeText(clipboardText);
 			}
-			
 		});
+
+		this.registerMarkdownPostProcessor((element, context) => {
+			const codeblocks = element.findAll("code");
+
+			for (const codeblock of codeblocks) {
+				const text = codeblock.innerText.trim();
+				console.log("text: " + text);
+				if (text[0] === config.openDelimiter && text[text.length - 1] === config.closeDelimiter) {
+					const tag = text.substring(1, text.length - 1); // Trim identifiers
+					const tagFound: boolean = GLOBAL_TAGS[tag] !== undefined;
+					const replaceEl = codeblock.createSpan({
+						text: tagFound ? GLOBAL_TAGS[tag] : text,
+						cls: tagFound ? "tag__success" : "tag__error",
+					});
+					console.log(replaceEl);
+					codeblock.replaceWith(replaceEl);
+				}
+			}
+		})
 	}
 
 	async onunload() {
